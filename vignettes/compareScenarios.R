2283ee379d528bbe654563a95422602187da966b
Falk Benke <falk.benke@pik-potsdam.de>
2024-04-26 19:04:08 +0200

    update tutorial

--- vignettes/compareScenarios.R
+++ vignettes/compareScenarios.R
@@ -6,6 +6,28 @@ knitr::opts_chunk$set(
 )
 
 ## -------------------------------------------------------------------------------------------------------------------------------------
+#  library(piamPlotComparison)
+#  compareScenarios(
+#    projectLibrary = "myProjectLibrary",
+#    mifScen = c("path/to/Base.mif", "path/to/NDC.mif"),
+#    mifHist = "path/to/historical.mif",
+#    outputFile = "CompareScenariosExample")
+
+## -------------------------------------------------------------------------------------------------------------------------------------
+#  library(piamPlotComparison)
+#  compareScenarios(
+#    projectLibrary = "myProjectLib",
+#    mifScen = c("path/to/scen1.mif", "path/to/scen2.mif", "path/to/scen3.mif"), # TODO.
+#    mifHist = "path/to/historical.mif", # TODO.
+#    outputDir = "path/to/where/rmds/should/be/copied/to", # TODO.
+#    outputFormat = "Rmd",
+#    # Add current time as output name to not overwrite other things:
+#    outputFile = format(Sys.time(), "CompScen2-%Y%m%d-%H%M%S"))
+
+## ----prepare mark---------------------------------------------------------------------------------------------------------------------
+#  # CLICK "RUN ALL CHUNKS ABOVE" HERE TO PREPARE THE ENVIRONMENT
+
+## -------------------------------------------------------------------------------------------------------------------------------------
 #  compareScenarios(
 #    projectLibrary = "myProjectLib",
 #    mifScen = c(newName1 = "path/to/scen1.mif", newName2 = "path/to/scen2.mif"),
